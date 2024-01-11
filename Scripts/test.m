0eb40bee5402e49081827b30ed601652137d2fce
Amreen Imrit <amimrit@gmail.com>
2024-01-11 00:57:42 -0500

    added describtion to evaluateSymbolic

--- Scripts/test.m
+++ Scripts/test.m
@@ -91,6 +91,16 @@ function plotEfficiecny (efficiency, t_val, theta, name)
 end
 
 function result = evaluateSymbolic (equations, val)
+    %{
+        take symbolic equationd and turn evalute over a specific range
+
+        Args:
+        equations (list) -> a list containing symbolic equations
+        val (double[]) -> a vector containing values over which we want to evaluate our symbolic function
+
+        Returns:
+        reulst (double[][]) -> 2D array, where each column is 1 euqations values over the range defined in val
+    %}
     syms t;
 
     result = zeros(numel(val), numel(equations));
