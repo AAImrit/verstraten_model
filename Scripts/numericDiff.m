b08662519a3256b325d61d6663a6f3ad49bad871
Amreen Imrit <amimrit@gmail.com>
2024-01-23 22:34:07 -0500

    debug numericDiff

--- Scripts/numericDiff.m
+++ Scripts/numericDiff.m
@@ -10,14 +10,14 @@ function derivative = numericDiff (y_val, x_val)
         derivative (double[]) -> the derivative 
     %}
     data_length = numel(x_val);
-    derivative = zeros(data_length);
+    derivative = zeros(data_length, 1);
 
     if (data_length < 3)
-        derivative(2) = (y_val(2)-y(1))/(x_val(2) - x(1));
+        derivative(2) = (y_val(2)-y_val(1))/(x_val(2) - x_val(1));
         return;
     end
 
-    derivative(1) = (y_val(1) - y(2))/( x_val(2) - x(1) ); %forward difference
-    derivative(data_length) = (y_val(data_length) - y(data_length-1)) / (x_val(data_length) - x(data_length-1)); %backward difference
-    derivative(2:data_length-1) = (y_val(1:data_length-2) - y(3:data_length))./(x_val(1:data_length-2) - x(3:data_length)); %central difference
+    derivative(1) = (y_val(1) - y_val(2))/( x_val(2) - x_val(1) ); %forward difference
+    derivative(data_length) = (y_val(data_length) - y_val(data_length-1)) / (x_val(data_length) - x_val(data_length-1)); %backward difference
+    derivative(2:data_length-1) =(y_val(1:data_length-2) - y_val(3:data_length))./(x_val(1:data_length-2) - x_val(3:data_length))'; %central difference
 end
